3ca314a6bedd030b3960cdb75e5ece63f66c98a7
Betsy Rowley <BRowley8@gmail.com>
2017-03-24 17:37:45 -0500

    Sets up basic file structure and starts server

---
+++ server/app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var port = 8000;
+
+app.use(express.static("server/public", {
+  index: "views/index.html"
+}));
+
+app.listen(port, function() {
+  console.log("Server listening on port ", port);
+});
